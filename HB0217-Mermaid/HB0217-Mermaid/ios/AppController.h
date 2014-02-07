e682790108bd2f5e0b3ef316cf66ba9239bc798c
wangusin <wangusin@b-a.com.tw>
2014-02-07 13:34:13 +0800

    新專案基本建立

---
+++ HB0217-Mermaid/HB0217-Mermaid/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  HB0217_MermaidAppController.h
+//  HB0217-Mermaid
+//
+//  Created by Mac-8 on 14/2/7.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ HB0217-Mermaid/HB0217-Mermaid/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  HB0217_MermaidAppController.h
+//  HB0217-Mermaid
+//
+//  Created by Mac-8 on 14/2/7.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
